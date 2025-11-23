bac70740e06d402ba60c31e2d2b8e91b75d59044
LoubnaEA <loubna.ea.a@gmail.com>
2025-11-23 14:54:18 +0100

    Local changes before rebase

---
+++ scripts/fate_engine/prophecy_generator.py
@@ -0,0 +1,5 @@
+\"\"\"Prophecy generation utilities for the fate_engine.\"\"\"
+
+def generate_prophecy(character):
+    \"\"\"Return a sample prophecy string for a given character.\"\"\"
+    return f\"{character} will face unexpected challenges.\"
---
+++ scripts/fate_engine/text_parser.py
@@ -0,0 +1,5 @@
+\"\"\"Text parsing utilities for the fate_engine.\"\"\"
+
+def parse_text(text):
+    \"\"\"Return a list of lines from the input text.\"\"\"
+    return text.splitlines()
